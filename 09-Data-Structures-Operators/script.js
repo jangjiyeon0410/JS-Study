e40206e43731ae4bf046b693b9f5317101281f74
jiyeonjang <jangjiyeon0410@gmail.com>
2023-01-14 16:13:55 +0900

    Rest pattern and Parameters

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -47,14 +47,50 @@ const restaurant = {
 			`Here is yout delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
 		);
 	},
+
+	orderPizza: function (mainIngredient, ...otherIngredients) {
+		console.log(mainIngredient);
+		console.log(otherIngredients);
+	},
 };
 
+// 1) Destructring
 // SPREAD, because on RIGHT side of =
 const arr = [1, 2, ...[3, 4]];
 
 // REST, because on LEFT side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5];
+const [a, b, ...others] = [1, 2, 3, 4, 5]; // 나머지를 다시 배열로
 console.log(a, b, others);
+
+const [pizza, , risotto, ...ohterFood] = [
+	...restaurant.mainMenu,
+	...restaurant.starterMenu,
+];
+console.log(pizza, risotto, ohterFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+	console.log(numbers);
+	let sum = 0;
+	for (let i = 0; i < numbers.length; i++) {
+		sum += numbers[i];
+		console.log(sum);
+	}
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 5);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushroom');
+
 /*
 /////////////////////////////////////
 // The Spread Operator (...)
