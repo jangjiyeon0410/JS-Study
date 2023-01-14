4bb709661b04e562e76131bf9d4b09f1229e8db5
jiyeonjang <jangjiyeon0410@gmail.com>
2023-01-14 22:11:03 +0900

    for-of loop

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/*
 // Data needed for a later exercise
 const flights =
 	'_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -54,6 +53,19 @@ const restaurant = {
 		console.log(otherIngredients);
 	},
 };
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+	// console.log(`${item[0] + 1}: ${item[1]}`);
+	console.log(`${i}: ${el}`);
+}
+
+// console.log([...menu.entries()]); // Arr ay iterator(반복자)
+
+/*
 
 const rest1 = {
 	name: 'Capri',
@@ -298,7 +310,7 @@ console.log(i, j, k);
 // Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
-*/
+
 
 // Cording Challenge #1
 const game = {
@@ -375,3 +387,4 @@ printGoals(...game.scored);
 // 7.
 team1 < team2 && console.log('Team1 is more likely to win');
 team1 > team2 && console.log('Team2 is more likely to win');
+*/
