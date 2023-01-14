239590475cc619179a9f9becf381ff524cfda721
jiyeonjang <jangjiyeon0410@gmail.com>
2023-01-14 15:35:52 +0900

    Destructing data, Spread Operator

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,6 +49,12 @@ const restaurant = {
 	},
 };
 
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
 /*
 /////////////////////////////////////
 // The Spread Operator (...)
