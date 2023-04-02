a8f0e214b983d26d1e47e3b5c3b9810a9d944e9b
Jiyeon <jangjiyeon0410@gmail.com>
2023-04-02 12:38:49 +0900

    0402

--- js_remind/practice.js
+++ js_remind/practice.js
@@ -1,5 +1,5 @@
 // settimeout, setinterval - 0331
-const h3 = document.getElementById('h3');
+// const h3 = document.getElementById('h3');
 
 // setTimeout(function(){
 //     h3.style.display = 'none';
@@ -48,10 +48,50 @@ lorem.addEventListener('scroll', function (){
     // const height = lorem.clientHeight;
     console.log(scrollTop)
     
-    scrollBar.style.width = `${scrollTop}%`
+    scrollBar.style.width = `${scrollTop*4}px`
 
     // if(scrollTop > height - 10){
     //     alert('읽기완료');
     // }
 
+});
+
+
+// tab menu - 0402
+const button = document.querySelectorAll('.button');
+const content = document.querySelectorAll('.content');
+
+for (let i =0; i<button.length; i++){
+    button[i].addEventListener('click', function (){
+        button.forEach(function(e){
+            console.log(e)
+            e.classList.remove('on');
+        });
+        button[i].classList.add('on');
+
+        content.forEach(function(e){
+            e.classList.remove('show');
+        });
+        content[i].classList.add('show');
+    });
+};
+
+// modal
+const modalBtn = document.querySelector('#modalBtn');
+const bg = document.querySelector('.bg');
+const modalPopup = document.querySelector('#modalPopup');
+
+modalBtn.addEventListener('click', function(){
+    modalPopup.classList.add('show');
+
+    // e.target // 유저가 실제로 누른 것
+    // e.currentTarget // 이벤트 리스너 달린 곳 -> this
+    // e.stopPropagation // 내 상위요소로 이벤트 버블링 막음
+});
+
+bg.addEventListener('click', function(e){
+    console.log(e.target)
+    if(e.target == bg){
+        modalPopup.classList.remove('show');
+    }
 });
